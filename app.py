a4f27c8e6ff5f3b66eddfe9ac7b2909ab69284be
Jose Ramirez <j.ramirez857@gmail.com>
2021-12-25 22:19:29 -0500

    added postgres migrations

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def index():
-    secret_key = app.config.get("SECRET_KEY")
-    return f"The configured secret key is {secret_key}."
+from models import Result
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
+if __name__ == '__main__':
+    app.run()
--- config.py
+++ config.py
@@ -2,15 +2,21 @@ import os
 
 class Config:
     DEBUG = False
-    DEVELOPMENT = False
+    TESTING = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
 
 class StagingConfig(Config):
+    DEVELOPMENT = True
     DEBUG = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
     DEVELOPMENT = True
+
+class TestingConfig(Config):
+    TESTING = True
